bca1e89a29863c9ae57b97313761aeed70439483
Yimin Chen <chenyiminch@gmail.com>
2017-07-10 10:38:19 +0800

    fix some minor errors

--- lib/datasets/album.py
+++ lib/datasets/album.py
@@ -99,7 +99,7 @@ class album(imdb):
 
         This function loads/saves from/to a cache file to speed up future calls.
         """
-        cache_file = os.path.join(self.cache_path, self.name + '_' + self._image_set + '_gt_roidb.pkl')
+        cache_file = os.path.join(self.cache_path, self.name + '_gt_roidb.pkl')
         if os.path.exists(cache_file):
             with open(cache_file, 'rb') as fid:
                 roidb = cPickle.load(fid)
